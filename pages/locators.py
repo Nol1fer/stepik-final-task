80a1ecf9889ce10837c393a91b0cd1f3daf16086
N0lifer <gluhov.m.n@gmail.com>
2025-04-12 08:14:24 +0500

    feat: move login page logic to base page

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators:
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
     BASKET_TOTAL = (By.CSS_SELECTOR, ".basket-mini")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
 from time import sleep
 
 import pytest
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @pytest.mark.parametrize(
     "link",
     [
@@ -47,6 +48,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     # sleep(5)
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -61,3 +63,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.should_not_be_success_message_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
